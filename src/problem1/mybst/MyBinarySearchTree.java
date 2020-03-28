c8b3c061f909ef307b4ac062e47f9916bc401868
amangarg24 <aman.garg_cs18@gla.ac.in>
2020-03-28 21:18:35 +0530

    problem 1 complete

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -1,50 +1,97 @@
 package problem1.mybst;
-// to implement BinarySearchTree
+
 import problem1.node.TreeNode;
+
+import java.util.ArrayList;
+
+// to implement BinarySearchTree
 public class MyBinarySearchTree {
-    public TreeNode root;
-    public int c=0;
-    public void insert(int value) {
-        TreeNode node = new TreeNode<>(value);
-        if ( root == null ) {
-            root = node;
-        }
-        else {
-            insertRec(root, node);
-        }
+
+    private TreeNode root;
+    private ArrayList<Integer> arrayList;
+
+    public MyBinarySearchTree() {
+        arrayList = new ArrayList<>();
     }
 
-    private void insertRec(TreeNode latestRoot, TreeNode node) {
-        if ( latestRoot.value > node.value) {
-            if ( latestRoot.left == null ) {
-                latestRoot.left = node;
-            }
-            else {
-                insertRec(latestRoot.left, node);
-            }
-        }
-        else{
-            if (latestRoot.right == null) {
-                latestRoot.right = node;
-            }
-            else{
-                insertRec(latestRoot.right, node);
-            }
-        }
+    public TreeNode getRoot() {
+        return root;
     }
-    public void print() {
-        printRec(root);
-        System.out.println("");
-        System.out.print(c);
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
     }
-    int i = 1;
-    private void printRec(TreeNode currentRoot) {
-        if(currentRoot==null) {
+
+    public void insert(TreeNode newNode) {
+
+        if (getRoot() == null) {
+            setRoot(newNode);
             return;
         }
-        System.out.println(i + " element::" +  currentRoot.value );
-        i++;
-        printRec(currentRoot.left);
-        printRec(currentRoot.right);
+
+        TreeNode parent = null;
+        TreeNode current = getRoot();
+        boolean isLeft = true;
+
+        while (current != null) {
+            parent = current;
+            if (newNode.getData() > current.getData()) {
+                current = current.getRightNode();
+                isLeft = false;
+            } else {
+                current = current.getLeftNode();
+                isLeft = true;
+            }
+        }
+        if (isLeft)
+            parent.setLeftNode(newNode);
+        else
+            parent.setRightNode(newNode);
+    }
+
+    public int showLeft(TreeNode node, boolean isLeft) {
+        int c = 0;
+
+        if (node == null)
+            return c;
+
+        if (isLeft)
+            System.out.println(node);
+
+        if (node.getLeftNode() == null)
+            c++;
+
+        c += showLeft(node.getLeftNode(), true);
+        c += showLeft(node.getRightNode(), false);
+        return c;
+    }
+
+    public void showTreeData(TreeNode node) {
+        if (node == null)
+            return;
+        System.out.println(node);
+        showTreeData(node.getLeftNode());
+        showTreeData(node.getRightNode());
+    }
+
+    private void addElementToList(TreeNode node) {
+        if (node == null)
+            return;
+        arrayList.add(node.getData());
+        addElementToList(node.getLeftNode());
+        addElementToList(node.getRightNode());
+    }
+
+    public ArrayList<Integer> getPreOrderList() {
+        addElementToList(getRoot());
+        return arrayList;
+    }
+
+    public void postOrderTraversal(TreeNode node) {
+        if (node == null)
+            return;
+        postOrderTraversal(node.getLeftNode());
+        postOrderTraversal(node.getRightNode());
+        System.out.println(node);
     }
 }
--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -1,13 +1,37 @@
 package problem1.node;
 
-public class TreeNode <T>{
-    public int value;
+public class TreeNode {
+    private int data;
+    private TreeNode leftNode;
+    private TreeNode rightNode;
 
-    public TreeNode left;
-
-    public TreeNode right;
-
-    public TreeNode(int value) {
-        this.value = value;
+    public int getData() {
+        return data;
     }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(TreeNode leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public TreeNode getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(TreeNode rightNode) {
+        this.rightNode = rightNode;
+    }
+
+    @Override
+    public String toString() {
+        return "data ==>> " + getData();
+    }
+
 }
